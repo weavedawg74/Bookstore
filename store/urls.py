cd2fdf3dd398fe9caa60c598f46a8ba639dcce0d
weavedawg74 <weavedawg74@yahoo.com>
2020-03-23 20:18:52 -0500

    modded store

--- store/urls.py
+++ store/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from store import views as store_view
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('store/', include('store.urls'), name='store'),
-    path('admin/', admin.site.urls),
+    path('', views.store, name='store'),
 ]
