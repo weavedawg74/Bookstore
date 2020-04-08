ec7eb876bb67912208f1ce2aaffb4148b4697dbc
weavedawg74 <weavedawg74@yahoo.com>
2020-04-08 18:47:47 -0500

    added field to the store such as stock, price, title

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
+ 
+class BookAdmin(admin.ModelAdmin):
+   list_display = ('title', 'author')
+ 
+admin.site.register(Book, BookAdmin)
+
 
 # Register your models here.
